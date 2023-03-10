381e05741541774ccd033a8d8c1247cf696c17b5
Lana Gordon <37781167+Larlar03@users.noreply.github.com>
2023-03-10 23:44:55 +0000

    Fixed tests

--- frontend/src/pages/Home/Home.test.tsx
+++ frontend/src/pages/Home/Home.test.tsx
@@ -2,51 +2,48 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import React from 'react';
 import Home from './Home';
-import App from '../../App';
+import { MemoryRouter as Router } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 
 describe('test button rendering', () => {
     const homePage = (
-        <App>
+        <Router>
             <Home />
-        </App>
+        </Router>
     );
-
     test('sign up button renders', () => {
         render(homePage);
-        const signUpButton = screen.getByText(/Sign Up/i);
+        const signUpButton = screen.getByText('Sign Up');
         expect(signUpButton).toBeInTheDocument();
     });
     test('log in button renders', () => {
         render(homePage);
-        const logInButton = screen.getByText(/Log In/i);
+        const logInButton = screen.getByText('Log In');
         expect(logInButton).toBeInTheDocument();
     });
 });
 
 describe('button navigating', () => {
     const homePage = (
-        <App>
+        <Router>
             <Home />
-        </App>
+        </Router>
     );
     test('sign up button navigates to sign up page', async () => {
         render(homePage);
 
-        const signUpButton = screen.getByText(/Sign Up/i);
+        const signUpButton = screen.getByText('Sign Up');
         await userEvent.click(signUpButton);
 
-        const pageLoad = screen.getByText(/Sign Up Page/i);
-        expect(pageLoad).toBeInTheDocument();
+        expect(signUpButton.closest('a')).toHaveAttribute('href', '/signup');
     });
 
     test('log in button navigates to login page', async () => {
         render(homePage);
 
-        const logInButton = screen.getByText(/Log In/i);
+        const logInButton = screen.getByText('Log In');
         await userEvent.click(logInButton);
 
-        const pageLoad = screen.getByText(/Log In Page/i);
-        expect(pageLoad).toBeInTheDocument();
+        expect(logInButton.closest('a')).toHaveAttribute('href', '/login');
     });
 });
